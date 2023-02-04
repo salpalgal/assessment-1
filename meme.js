18bb6639cc77d5e7b8bdd8eba13865992a67c471
sally wang <sal@LAPTOP-66V3CAS9.localdomain>
2023-02-04 15:33:59 -0800

    modular functions

--- meme.js
+++ meme.js
@@ -3,49 +3,111 @@ const form=document.querySelector("form")
 const image=document.querySelector("#imageUrl")
 const topText=document.querySelector("#topText")
 const bottomText=document.querySelector("#bottomText")
-let div=document.querySelector("#memeDisplay")
+let bodyDiv=document.querySelector("#memeDisplay")
 let body=document.querySelector("body")
+// let memeContainer= document.createElement("div")
+// memeContainer.setAttribute("id","memeContain")
+// let div = document.createElement("div")
+// div.setAttribute("id","memeDiv")
+
+
+function createContainer(e){
+    e.preventDefault()
+    let memeContainer = document.createElement("div")
+    memeContainer.setAttribute("id","memeContain")
+    bodyDiv.append(memeContainer)
+}
+
+
 
 function topCaptionFunc(e){
-   
-    let topCaption= document.createElement("div")
-    topCaption.innerText=topText.value
-    topCaption.setAttribute("id","top")
-}
+    e.preventDefault()
+     let topCaption= document.createElement("div")
+     topCaption.innerText=topText.value
+    //  let memeContainer= document.createElement("div")
+    //  memeContainer.setAttribute("id","memeContain")
+     let memeContainer = document.querySelector("#memeContain")
+     
+     memeContainer.append(topCaption)
+     topCaption.setAttribute("id","top")
+    //  div.append(topCaption)
+
+     
+     topText.value=""
+     
+ 
+ }
+
 function bottomCaptionFunc(e){
-   
+   e.preventDefault()
     let bottomCaption = document.createElement("div")
     bottomCaption.innerText=bottomText.value
+    // let memeContainer= document.createElement("div")
+    // memeContainer.setAttribute("id","memeContain")
+    let memeContainer = document.querySelector("#memeContain")
+    memeContainer.append(bottomCaption)
     bottomCaption.setAttribute("id","bottom")
+    // div.append(bottomCaption)
+    bottomText.value=""
 }
-function memeContainFunc(e){
-   
-    let memeContain= document.createElement("div")
+function appendImage(e){
+    e.preventDefault()
     let img= document.createElement("img")
     img.src=image.value
-    memeContain.append(img)
-    memeContain.append(topCaptionFunc)
-    memeContain.append(bottomCaptionFunc)
-    memeContain.setAttribute("id","memeContain")
+    // let memeContainer= document.createElement("div")
+    // memeContainer.setAttribute("id","memeContain")
+    let memeContainer = document.querySelector("#memeContain")
+     
+    memeContainer.append(img)
+    // div.append(img)
+    // div.append(memeContainer)
+    // div.appendChild(memeContainer)
+    image.value=""
     
-    memeContain.append(removeFunc)
+   
 }
+
 function removeFunc(e){
+    e.preventDefault()
     let remove=document.createElement("button")
     remove.innerText="remove"
+    // let memeContainer= document.createElement("div")
+    // memeContainer.setAttribute("id","memeContain")
+    let memeContainer = document.querySelector("#memeContain")
+    memeContainer.append(remove)
+    // div.appendChild(memeContainer)
     remove.setAttribute("id","remove")
+    // div.append(remove)
 }
 
-form.addEventListener("submit",(e)=>{
+function appendToBody(e){
     e.preventDefault();
-    div.append(memeContainFunc)
-    image.value=""
-    topText.value=""
-    bottomText.value=""
-})
+    const memeContainer= document.querySelector("#memeContain")
+    bodyDiv.append(memeContainer)
+    }
+
+// form.addEventListener("submit",appendMeme)
+form.addEventListener("submit",createContainer)
+form.addEventListener("submit",appendToBody)
+form.addEventListener("submit",appendImage)
+form.addEventListener("submit", topCaptionFunc)
+form.addEventListener("submit",bottomCaptionFunc)
+form.addEventListener("submit",removeFunc)
 
 
-div.addEventListener("click",function(e){
+
+// form.addEventListener("submit", function (e){
+//     e.preventDefault()
+//     ;
+    // image.value=""
+    // topText.value=""
+    // bottomText.value=""
+// })
+    
+
+
+
+bodyDiv.addEventListener("click",function(e){
     if(e.target.tagName==="BUTTON"){
         e.target.parentElement.remove()
     }
---
+++ meme.test.js
@@ -0,0 +1,5 @@
+it('should create a div and append to the div with ID of #memeDisplay',function(){
+    const memeDisplay= document.getElementById("memeDisplay")
+    expect(memeDisplay.length).toEqual(1)
+}
+)
